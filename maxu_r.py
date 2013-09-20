8a6d73b5c725e1284a910d2775b2a7440c37e491
Kityan Choi <k1c@k1c.(none)>
2013-09-20 09:47:00 -0400

    Fixed bug in ENSO/maxu_r where runaveu was not used.  Automatically fix even number window in util.nc.runave

--- maxu_r.py
+++ maxu_r.py
@@ -11,8 +11,8 @@ def max_anom(u,lon_width=40.,lat=(-5.,5.)):
     lat - region in the latitude (default = (-5.,5.))
     '''
     nlon = numpy.round(lon_width/numpy.diff(u.getLongitude()[0:2])[0])
-    runaveu = u.getRegion(lat=lat).wgt_ave('Y').squeeze().runave(nlon,'X')
-    return u.data[numpy.abs(u.data) == numpy.abs(u.data).max()][0]
+    runaveu = u.getRegion(lat=lat).wgt_ave('Y').runave(nlon,'X').data.squeeze()
+    return runaveu[numpy.abs(runaveu) == numpy.abs(runaveu).max()][0]
 
 def plot_r(y,x,*args,**kwargs):
     ''' Read a list of variable, apply maxu_anom to each of them
@@ -31,10 +31,12 @@ def plot_r(y,x,*args,**kwargs):
     x = numpy.append(x,0.)
     
     assert len(y) == len(x)
-    
+    print x,y
     s_neg = numpy.polyfit(x[x<=0],y[x<=0],1)[0]
     s_pos = numpy.polyfit(x[x>=0],y[x>=0],1)[0]
+
     pylab.plot(numpy.sort(x),numpy.sort(y),*args,**kwargs)
+    pylab.scatter(x,y)    
     return (s_pos - s_neg)/(s_pos + s_neg)
 
 
