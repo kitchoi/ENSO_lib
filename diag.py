a3a39e9481f48a204e7af0d068d57e46aeb4bdec
Kit-Yan Choi <kityanc@mara.Princeton.EDU>
2015-05-14 18:48:33 -0400

    * ENSO/diag.py: added ENSO_transition2 which agrees with previous work,                 added kwargs in findENSO_percentile

--- diag.py
+++ diag.py
@@ -1,22 +1,31 @@
 import numpy
 import warnings
+import itertools
 
 import util.stat as stat
 import util
 
 
-def findENSO_percentile(index,percentile):
+def findENSO_percentile(index,percentile,*args,**kwargs):
     ''' percentile < 50.
     warm,cold = findENSO_percentile(nino34,15.)
     warm and cold are dictionaries containing the locations and peak values of the events
     '''
     if percentile > 50.: percentile = 100. - percentile
+    if percentile < 0.:
+        raise ValueError("percentile cannot be smaller than zero.")
+    if percentile > 100.:
+        raise ValueError("percentile cannot be bigger than 100.")
+    
     warm = {}
     cold = {}
-    if not isinstance(index,numpy.ma.core.MaskedArray):
-        index = numpy.ma.array(index)
-    warm['locs'],warm['peaks'] = findEvents(index,'>',numpy.percentile(index[index.mask==0],100-percentile))
-    cold['locs'],cold['peaks'] = findEvents(index,'<',numpy.percentile(index[index.mask==0],percentile))
+    if numpy.ma.isMaskedArray(index):
+        if index.mask.any():
+            warm['locs'],warm['peaks'] = findEvents(index,'>',numpy.percentile(index[~index.mask],100-percentile),*args,**kwargs)
+            cold['locs'],cold['peaks'] = findEvents(index,'<',numpy.percentile(index[~index.mask],percentile),*args,**kwargs)
+            return warm,cold
+    warm['locs'],warm['peaks'] = findEvents(index,'>',numpy.percentile(index,100-percentile),*args,**kwargs)
+    cold['locs'],cold['peaks'] = findEvents(index,'<',numpy.percentile(index,percentile),*args,**kwargs)
     return warm,cold
 
 
@@ -155,44 +164,193 @@ def find_EN_pattern(field,nino34,nino34_mid=0.8,nino34_tole=0.4,
     return pattern
 
 
-def ENSO_duration(nino34,percentile,thres_std_fraction):
-    warm,cold = findENSO_percentile(nino34,percentile)
-    warm_end = nino34.std()*thres_std_fraction
-    cold_end = warm_end*-1
-    duration = {}
-    
-    def compute_duration(operator,locs,evt_end):
+def compute_duration(nino34,operator,locs,evt_end,remove_merge_event=True):
         lengths = []
         for iloc in xrange(len(locs)):
             loc = locs[iloc]
             after_end = operator(nino34[loc:],evt_end)
             if after_end.any():
                 length =  numpy.where(after_end)[0][0]
-                if iloc < len(locs)-1:
-                    # Avoid double counting events that reintensify
-                    if locs[iloc+1]-locs[iloc] > length:
-                        lengths.append(length)
+                if remove_merge_event:
+                    if iloc < len(locs)-1:  # if this is not the last event peak
+                        if locs[iloc+1]-locs[iloc] > length:
+                             lengths.append(length) # Only count the duration 
+                             # if the next event occurs after
+                             # the termination
+                else:
+                    lengths.append(length)
         return lengths
     
-    duration['warm'] = compute_duration(numpy.less,warm['locs'],warm_end)
-    duration['cold'] = compute_duration(numpy.greater,cold['locs'],cold_end)
+
+
+def ENSO_duration(nino34,percentile,thres_std_fraction,per=5,window=[-3,3]):
+    ''' compute the duration of the warm and cold events
+    Input:
+    nino34             - numpy 1D array
+    percentile         - 0. < percentile < 100.
+    thres_std_fraction - the fraction times the nino34 standard deviation 
+                         is used for defining the termination
+    
+    Return : A dictionary with keys "warm" and "cold"
+             each contains a list of integers which
+             are the duration of the events
+    '''
+    warm,cold = findENSO_percentile(nino34,percentile,per,window)
+    warm_end = nino34.std()*thres_std_fraction
+    cold_end = warm_end*-1
+    duration = {}
+    duration['warm'] = compute_duration(nino34,numpy.less,
+                                        warm['locs'],warm_end)
+    duration['cold'] = compute_duration(nino34,numpy.greater,
+                                        cold['locs'],cold_end)
+    duration['warm_locs'] = warm['locs']
+    duration['cold_locs'] = cold['locs']
+    # Length of duration['warm'] may not match the length of 
+    # duration['warm_locs'] as double counting is taken care of
     return duration
 
 
-def ENSO_transition(nino34,percentile,wait_window,per=5,window=[-3,3]):
-    warm,cold = findENSO_percentile(nino34,percentile)
-    events = ['warm',]*len(warm['locs'])+['cold',]*len(cold['locs'])
-    events_locs = sorted(zip(events,warm['locs']+cold['locs']),key=lambda v:v[1])
-    events = [ evt for (evt,loc) in events_locs ]
-    locs = [ loc for (evt,loc) in events_locs ]
+def ENSO_transition2(nino34,percentile,wait_window,thres_std_fraction,
+                     per=5,window=[-3,3]):
+    ''' Word for word copy from the Matlab code
+    Compute the transition probabilities
+    TODO: should be made more elegant
+    '''
+    ENLNind = []
+    LNENind = []
+    ENENind = []
+    LNLNind = []
+    warm,cold = findENSO_percentile(nino34,percentile,per,window)
+    warm['locs'] = numpy.array(sorted(warm['locs']))
+    cold['locs'] = numpy.array(sorted(cold['locs']))
+    warm_end = nino34.std()*thres_std_fraction
+    cold_end = warm_end*-1
+    for i,iwarm in enumerate(warm['locs']):
+        ended = numpy.where(nino34[iwarm:] < warm_end)[0]
+        if len(ended):
+            # The index where it terminates
+            iterm = iwarm + ended[0]
+        nextcold = numpy.where(cold['locs']>iwarm)[0]
+        nextwarm = numpy.where(warm['locs']>iwarm)[0]
+        if len(nextcold) and len(nextwarm):
+            inextcold = cold['locs'][nextcold[0]]
+            inextwarm = warm['locs'][nextwarm[0]]
+            if (inextcold - iterm) <= wait_window:
+                ENLNind.append(iwarm)
+            elif (inextwarm - iterm) <= wait_window:
+                ENENind.append(iwarm)
+        elif len(nextcold):
+            if (cold['locs'][nextcold[0]] - iterm) <= wait_window:
+                ENLNind.append(iwarm)
+        elif len(nextwarm):
+            if (warm['locs'][nextwarm[0]] - iterm) <= wait_window:
+                ENENind.append(iwarm)
+    for i,icold in enumerate(cold['locs']):
+        ended = numpy.where(nino34[icold:] > cold_end)[0]
+        if len(ended):
+            # The index where it terminates
+            iterm = icold + ended[0]
+        nextwarm = numpy.where(warm['locs']>icold)[0]
+        nextcold = numpy.where(cold['locs']>icold)[0]
+        if len(nextwarm) and len(nextcold):
+            inextwarm = warm['locs'][nextwarm[0]]
+            inextcold = cold['locs'][nextcold[0]]
+            if (inextwarm - iterm) <= wait_window:
+                LNENind.append(icold)
+            elif (inextcold - iterm) <= wait_window:
+                LNLNind.append(icold)
+        elif len(nextwarm):
+            if (warm['locs'][nextwarm[0]] - iterm) <= wait_window:
+                LNENind.append(icold)
+        elif len(nextcold):
+            if (cold['locs'][nextcold[0]] - iterm) <= wait_window:
+                LNLNind.append(icold)
+    nEN = len(warm['locs'])
+    nLN = len(cold['locs'])
+    if nEN and nLN:
+        if warm['locs'][-1] > cold['locs'][-1]:
+            # warm event the last
+            nEN -= 1
+        else:
+            nLN -= 1
+    elif nEN:
+        nEN -= 1
+    elif nLN:
+        nLN -= 1
+    transition = {}
+    if nEN:
+        transition['warm_cold'] = float(len(ENLNind))/nEN
+        transition['warm_warm'] = float(len(ENENind))/nEN
+    if nLN:
+        transition['cold_warm'] = float(len(LNENind))/nLN
+        transition['cold_cold'] = float(len(LNLNind))/nLN
+    return transition,dict(ENLN=ENLNind,ENEN=ENENind,
+                           LNEN=LNENind,LNLN=LNLNind)
+
+
+
+def ENSO_transition(nino34,percentile,wait_window,thres_std_fraction,
+                    per=5,window=[-3,3]):
+    warm,cold = findENSO_percentile(nino34,percentile,per,window)
+    warm_end = nino34.std()*thres_std_fraction
+    cold_end = warm_end*-1
+    durations = {'warm': compute_duration(nino34,numpy.less,
+                                          warm['locs'],warm_end,False),
+                 'cold': compute_duration(nino34,numpy.greater,
+                                          cold['locs'],cold_end,False),}
+    events = ['warm',]*len(warm['locs'])+\
+             ['cold',]*len(cold['locs'])
+    events,locs,termT = zip(*sorted(
+        zip(events,
+            warm['locs']+cold['locs'],
+            durations['warm']+durations['cold']),
+        key=lambda v:v[1]))
     transition = {}
     for iloc in xrange(len(events)-1):
         evt_pair = '_'.join(events[iloc:iloc+2])
-        if locs[iloc+1]-locs[iloc] <= wait_window:
+        if (locs[iloc+1] - locs[iloc] - termT[iloc]) <= wait_window:
             transition[evt_pair] = transition.setdefault(evt_pair,0) + 1
+    # warm,cold = findENSO_percentile(nino34,percentile,per,window)
+    # events = ['warm',]*len(warm['locs'])+['cold',]*len(cold['locs'])
+    # events,locs = zip(*sorted(
+    #     zip(events,warm['locs']+cold['locs']),key=lambda v:v[1]))
+    # transition = {}
+    # for iloc in xrange(len(events)-1):
+    #     evt_pair = '_'.join(events[iloc:iloc+2])
+    #     if locs[iloc+1]-locs[iloc] <= wait_window:
+    #         transition[evt_pair] = transition.setdefault(evt_pair,0) + 1
     transition['warm'] = len(warm['locs'])
     transition['cold'] = len(cold['locs'])
+    transition['warm_locs'] = warm['locs']
+    transition['cold_locs'] = warm['locs']
     return transition
+
+
+def ENSO_transition_prob(*args,**kwargs):
+    tra = ENSO_transition(*args,**kwargs)
+    nEN,nLN = tra['warm'],tra['cold']
+    Prob = {}
+    if tra['warm_locs'] and tra['cold_locs']:
+        if tra['warm_locs'][-1] > tra['cold_locs'][-1]:
+            # warm comes last
+            nEN -= 1
+        else:
+            # cold comes last
+            nLN -= 1
+    elif tra['warm_locs']:
+        # warm comes last
+        nEN -= 1
+    elif tra['cold_locs']:
+        # cold comes last
+        nLN -= 1
+    for phase1,phase2 in itertools.product(['warm','cold'],['warm','cold']):
+        tra_phase = phase1+"_"+phase2
+        if phase1 == 'warm': 
+            n = nEN
+        else:
+            n = nLN
+        Prob[tra_phase] = float(tra.get(tra_phase,0.))/n
+    return Prob
 
 
 def ENSO_skewness(nino34):
